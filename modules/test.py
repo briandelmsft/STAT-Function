b5caa91da3b50e4147408ca95a2ee1558da8ca49
Brian Delaney <briandel@microsoft.com>
2023-05-08 17:29:06 -0400

    return highest function

--- modules/test.py
+++ modules/test.py
@@ -3,27 +3,37 @@ from shared import rest, data
 import json
 
 def execute_test_module (req_body):
-    base_object = BaseModule()
-    base_object.load_from_input(req_body['BaseModuleBody'])
+    #base_object = BaseModule()
+    #base_object.load_from_input(req_body['BaseModuleBody'])
     #test = base_object.get_ip_list()
     #test2 = base_object.get_account_id_list()
     #test3 = base_object.get_account_upn_list()
-    ip_entities = base_object.get_ip_kql_table()
-    account_entities = base_object.get_account_kql_table()
+    #ip_entities = base_object.get_ip_kql_table()
+    #account_entities = base_object.get_account_kql_table()
 
-    if req_body['AddIncidentComments']:
-        try:
-            test = 5 / 0
-        except:
-            raise STATError('Cannot divide by zero', {'error': 'division by zero error'}, 400)
+    list_data = req_body['ListData']
+    item_key = req_body['Key']
+
+    if req_body.get('SortOrder'):
+        out = data.return_highest_value(list_data, item_key, req_body.get('SortOrder'))
     else:
-        test = 5 / 0
+        out = data.return_highest_value(list_data, item_key)
 
-    data.list_to_html_table(base_object.IPs)
+    test = 1
+
+    # if req_body['AddIncidentComments']:
+    #     try:
+    #         test = 5 / 0
+    #     except:
+    #         raise STATError('Cannot divide by zero', {'error': 'division by zero error'}, 400)
+    # else:
+    #     test = 5 / 0
+
+    # data.list_to_html_table(base_object.IPs)
 
     #query = ip_entities + account_entities + '''union ipEntities,accountEntities'''
 
     #results = rest.execute_la_query(base_object.WorkspaceId, query, 7)
 
 
-    return Error({'Error': 'Test completed successfully'})
+    return Error({'Error': f'Test completed successfully: {out}'})
--- shared/data.py
+++ shared/data.py
@@ -1,8 +1,22 @@
 import pandas as pd
 
-def list_to_html_table(list:list, max_rows=20, max_cols=10):
-    df = pd.DataFrame(list)
+def list_to_html_table(input_list:list, max_rows=20, max_cols=10):
+    '''Convert a list of dictionaries into an HTML table'''
+    df = pd.DataFrame(input_list)
     df.index = df.index + 1
     html_table = df.to_html(max_rows=max_rows, max_cols=max_cols).replace('\n', '')
 
     return html_table
+
+def return_highest_value(input_list:list, key:str, order:list=['High','Medium','Low','Informational','Unknown']):
+    '''Locate the highest value in a list of dictionaries by key'''
+    
+    unsorted_list = []
+    for item in input_list:
+        unsorted_list.append(item[key].lower())
+
+    for item in order:
+        if item.lower() in unsorted_list:
+            return item
+    
+    return 'Unknown'
