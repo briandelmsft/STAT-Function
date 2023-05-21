0b8254d742568f2a1ed591e40c45f6abb69bcee9
Pierre <piaudonn@microsoft.com>
2023-05-21 18:19:22 -0400

    Add None to the default list

--- shared/data.py
+++ shared/data.py
@@ -8,7 +8,7 @@ def list_to_html_table(input_list:list, max_rows=20, max_cols=10, nan_str='N/A')
 
     return html_table
 
-def return_highest_value(input_list:list, key:str, order:list=['High','Medium','Low','Informational','Unknown']):
+def return_highest_value(input_list:list, key:str, order:list=['High','Medium','Low','Informational','None','Unknown']):
     '''Locate the highest value in a list of dictionaries by key'''
     
     unsorted_list = []
