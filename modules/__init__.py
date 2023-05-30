9758745607392f11274eb4fdfb09e8caa5b999ed
Brian Delaney <briandel@microsoft.com>
2023-05-30 09:54:45 -0400

    error handling

--- modules/__init__.py
+++ modules/__init__.py
@@ -24,7 +24,7 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     except Exception as e:
         trace = ''.join(tb.format_exception(None, e, e.__traceback__))
         logging.error(e, exc_info=True)
-        return func.HttpResponse(json.dumps({'Error': e.args, 'InvocationId': context.invocation_id, 'Traceback': trace}), status_code=400, mimetype='application/json')
+        return func.HttpResponse(json.dumps({'Error': 'Module processing failed, an unknown exception has occurred.', 'InvocationId': context.invocation_id, 'Traceback': trace}), status_code=400, mimetype='application/json')
     except:
         logging.error(msg={'Error': 'Module processing failed, an unknown exception has occurred.', 'InvocationId': context.invocation_id}, exc_info=True)
         return func.HttpResponse(json.dumps({'Error': 'Module processing failed, an unknown exception has occurred.', 'InvocationId': context.invocation_id}), status_code=400, mimetype='application/json')
