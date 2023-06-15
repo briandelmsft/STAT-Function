2bc1bf502c7d70b3578edcb993e5580853bb5800
Brian Delaney <briandel@microsoft.com>
2023-06-14 20:42:32 -0400

    updated tests

--- tests/test_stat.py
+++ tests/test_stat.py
@@ -8,11 +8,28 @@ def test_base_module_incident():
 
     assert base_response.statuscode == 200
     assert base_response.body.AccountsCount == 2
+    assert len(base_response.body.Accounts) == base_response.body.AccountsCount
+    assert len(base_response.body.Domains) == base_response.body.DomainsCount
+    assert len(base_response.body.FileHashes) == base_response.body.FileHashesCount
+    assert len(base_response.body.Files) == base_response.body.FilesCount
+    assert len(base_response.body.Hosts) == base_response.body.HostsCount
+    assert len(base_response.body.IPs) == base_response.body.IPsCount
+    assert len(base_response.body.URLs) == base_response.body.URLsCount
+    assert len(base_response.body.OtherEntities) == base_response.body.OtherEntitiesCount
+
 
 def test_base_module_alert():
     base_response:Response = base.execute_base_module(get_alert_trigger_data())
 
     assert base_response.statuscode == 200
+    assert len(base_response.body.Accounts) == base_response.body.AccountsCount
+    assert len(base_response.body.Domains) == base_response.body.DomainsCount
+    assert len(base_response.body.FileHashes) == base_response.body.FileHashesCount
+    assert len(base_response.body.Files) == base_response.body.FilesCount
+    assert len(base_response.body.Hosts) == base_response.body.HostsCount
+    assert len(base_response.body.IPs) == base_response.body.IPsCount
+    assert len(base_response.body.URLs) == base_response.body.URLsCount
+    assert len(base_response.body.OtherEntities) == base_response.body.OtherEntitiesCount
 
 def test_related_alerts():
     alerts_input = {
@@ -29,6 +46,8 @@ def test_related_alerts():
     alerts_response:Response = relatedalerts.execute_relatedalerts_module(alerts_input)
 
     assert alerts_response.statuscode == 200
+    assert alerts_response.body.RelatedAlertsFound == True
+    assert alerts_response.body.RelatedAccountAlertsFound == True
 
 def test_threat_intel():
     ti_input = {
