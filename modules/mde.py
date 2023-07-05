61f0faaaf81741ca8d72ad5f6888ff51a4ae3c89
Pierre Audonnet <piaudonn@microsoft.com>
2023-07-04 23:26:28 -0400

    Create mde.py
    No support for Tasks

---
+++ modules/mde.py
@@ -0,0 +1,119 @@
+from classes import BaseModule, Response, MDEModule
+from shared import rest, data
+import json
+
+def execute_mde_module (req_body):
+
+    #Inputs AddIncidentComments, AddIncidentTask, Entities, IncidentTaskInstructions
+
+    base_object = BaseModule()
+    base_object.load_from_input(req_body['BaseModuleBody'])
+    lookback = req_body.get('LookbackInDays', 7)
+    mde_object = MDEModule()
+
+    detailed_accounts = []
+    for account in base_object.Accounts:
+        usersid = account.get('onPremisesSecurityIdentifier')
+        if usersid:
+            userid = account.get('id')
+            userupn = account.get('userPrincipalName')
+            current_account = {
+                'UserDevices': [],
+                'UserHighestExposureLevel': 'Unknown',
+                'UserHighestRiskScore': 'Unknown',
+                'UserId': f'{userid}',
+                'UserPrincipalName': f'{userupn}',
+                'UserSid': f'{usersid}'
+            }
+            get_devices = ('DeviceLogonEvents'
+                        f'| where Timestamp > ago({lookback}d)'
+                        f'| where AccountSid =~ "{usersid}"'
+                        '| where LogonType in ("Interactive","RemoteInteractive")'
+                        '| distinct DeviceName, DeviceId')
+            results = rest.execute_m365d_query(base_object, get_devices)
+            if results:
+                current_account['UserDevices'] = []
+                #Split the results into chuncks of 30 in case there are many devices assocaited with that users 
+                max_device_per_query = 30
+                splited_results = [results[i:i+max_device_per_query] for i in range(0, len(results), max_device_per_query)]
+                for result_chunck in splited_results:
+                    idlist = ','.join(['"'+item['DeviceId']+'"' for item in result_chunck])
+                    pathwithfilter = f'/api/machines?$filter=id+in+({idlist})&$select=id,computerDnsName,riskScore,exposureLevel'
+                    devicedata = json.loads(rest.rest_call_get(base_object, 'mde', f'{pathwithfilter}').content)
+                    if len(devicedata['value']) > 0:
+                        current_account['UserDevices'] += devicedata['value']
+            current_account['UserHighestExposureLevel'] = data.return_highest_value(current_account['UserDevices'],'exposureLevel')
+            current_account['UserHighestRiskScore'] = data.return_highest_value(current_account['UserDevices'],'riskScore') 
+            detailed_accounts.append( current_account) 
+    mde_object.DetailedResults['Accounts'] = detailed_accounts
+    mde_object.UsersHighestExposureLevel = data.return_highest_value(mde_object.DetailedResults['Accounts'],'UserHighestExposureLevel') 
+    mde_object.UsersHighestRiskScore = data.return_highest_value(mde_object.DetailedResults['Accounts'],'UserHighestRiskScore')
+    
+    detailed_hosts = []
+    for host in base_object.Hosts:
+        hostmdeid = host.get('MdatpDeviceId')
+        hostfqdn = host.get('FQDN')
+        if hostmdeid or hostfqdn:
+            if hostmdeid:
+                queryfilter = f"id+eq+'{hostmdeid}'"
+            else:
+                queryfilter = f"computerDnsName+eq+'{hostfqdn}'"
+            pathwithfilter = f"/api/machines?$filter={queryfilter}&$select=id,computerDnsName,riskScore,exposureLevel"
+            devicedata = json.loads(rest.rest_call_get(base_object, 'mde', f'{pathwithfilter}').content)
+            if len(devicedata['value']) > 0:
+                detailed_hosts += devicedata['value']
+    mde_object.DetailedResults['Hosts'] = detailed_hosts
+    mde_object.HostsHighestExposureLevel = data.return_highest_value(mde_object.DetailedResults['Hosts'],'exposureLevel') 
+    mde_object.HostsHighestRiskScore = data.return_highest_value(mde_object.DetailedResults['Hosts'],'riskScore')
+    
+    detailed_ips = []
+    for ip in base_object.IPs:
+        ipaddress = ip.get('Address')
+        get_devices = ('DeviceNetworkInfo'
+                    f'| where Timestamp > ago({lookback}d)'
+                    '| summarize arg_max(Timestamp,*) by DeviceId'
+                    '| extend IPs = todynamic(IPAddresses)'
+                    '| mv-expand IPs'
+                    '| evaluate bag_unpack(IPs)'
+                    f'| where IPAddresses == "{ipaddress}"'
+                    '| distinct IPAddress, DeviceId'
+                    '| top 30 by DeviceId') #Only returns 30 devices
+        results = rest.execute_m365d_query(base_object, get_devices)
+        if results:
+            idlist = ','.join(['"'+item['DeviceId']+'"' for item in results])
+            pathwithfilter = f'/api/machines?$filter=id+in+({idlist})&$select=id,computerDnsName,riskScore,exposureLevel'
+            devicedata = json.loads(rest.rest_call_get(base_object, 'mde', f'{pathwithfilter}').content)
+            if len(devicedata['value']) > 0:
+                detailed_ips += devicedata['value']
+    mde_object.DetailedResults['IPs'] = detailed_ips
+    mde_object.IPsHighestExposureLevel = data.return_highest_value(mde_object.DetailedResults['IPs'],'exposureLevel') 
+    mde_object.IPsHighestRiskScore = data.return_highest_value(mde_object.DetailedResults['IPs'],'riskScore')
+
+    nb_accounts = len(mde_object.DetailedResults['Accounts'])
+    nb_hosts = len(mde_object.DetailedResults['Hosts'])
+    nb_ips = len(mde_object.DetailedResults['IPs'])
+    entities_nb = nb_accounts + nb_hosts + nb_ips
+    if entities_nb != 0:
+        mde_object.AnalyzedEntities = entities_nb
+
+    if req_body.get('AddIncidentComments', False):
+        comment = f'<h3>Microsoft Defender for Endpoint Module</h3>'
+        comment += f'A total of {mde_object.AnalyzedEntities} entities were analyzed (Accounts: {nb_accounts} - Hosts: {nb_hosts} - IPs: {nb_ips}).<br />'
+        if nb_accounts > 0:
+            html_table_accounts = data.list_to_html_table( [{k: v for k, v in DetailedResults.items() if k != 'UserDevices'} for DetailedResults in mde_object.DetailedResults['Accounts']])
+            comment += f'<ul><li>Maximum Risk Score of devices used by the user entities: {mde_object.UsersHighestRiskScore}</li>'
+            comment += f'<li>Maximum Exposure Level of devices used by the user entities: {mde_object.UsersHighestExposureLevel}</li></ul>'
+            comment += f'{html_table_accounts}'
+        if nb_hosts > 0:
+            html_table_hosts = data.list_to_html_table(mde_object.DetailedResults['Hosts'])
+            comment += f'<ul><li>Maximum Risk Score of devices present in the incident: {mde_object.HostsHighestRiskScore}</li>'
+            comment += f'<li>Maximum Exposure Level of devices present in the incident: {mde_object.HostsHighestExposureLevel}</li></ul>'
+            comment += f'{html_table_hosts}'
+        if nb_ips > 0:
+            html_table_ips = data.list_to_html_table(mde_object.DetailedResults['IPs'])
+            comment += f'<ul><li>Maximum Risk Score of IPs present in the incident: {mde_object.IPsHighestRiskScore}</li>'
+            comment += f'<li>Maximum Exposure Level of IPs present in the incident: {mde_object.IPsHighestExposureLevel}</li></ul>'
+            comment += f'{html_table_ips}'
+        comment_result = rest.add_incident_comment(base_object, comment)
+
+    return Response(mde_object)
