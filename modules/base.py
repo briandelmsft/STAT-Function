53e2acc1b884405042baf66451ca13826fd9d9ab
Brian Delaney <briandel@microsoft.com>
2023-07-05 11:06:53 -0400

    add mde device id to host when available

--- modules/base.py
+++ modules/base.py
@@ -321,8 +321,9 @@ def enrich_hosts(entities):
     for host in host_entities:
         host_name = data.coalesce(host.get('properties',{}).get('hostName'), host.get('HostName'))
         domain_name = data.coalesce(host.get('properties',{}).get('dnsDomain'), host.get('DnsDomain'))
+        mde_device_id = data.coalesce(host.get('properties',{}).get('additionalData', {}).get('MdatpDeviceId'), host.get('MdatpDeviceId'))
         raw_entity = data.coalesce(host.get('properties'), host)
-        base_object.add_host_entity(fqdn=host_name + '.' + domain_name, hostname=host_name, dnsdomain=domain_name, rawentity=raw_entity)
+        base_object.add_host_entity(fqdn=host_name + '.' + domain_name, hostname=host_name, dnsdomain=domain_name, mdedeviceid=mde_device_id, rawentity=raw_entity)
 
 def get_account_comment():
     
