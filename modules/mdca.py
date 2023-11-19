9b2124eec837a44656de980bac752076695ba711
Pierre Audonnet <piaudonn@microsoft.com>
2023-11-19 15:08:25 -0500

    Update mdca.py
    Adding insights into comments
    Fixing user link

--- modules/mdca.py
+++ modules/mdca.py
@@ -67,13 +67,16 @@ def execute_mdca_module (req_body):
         mdac_object.AnyThreatScoreTrendingUp = any(trend.get('ThreatScoreTrendingUp') == True for trend in mdac_object.DetailedResults)
 
     if req_body.get('AddIncidentComments', True):
-        link_template = f'<a href="https://security.microsoft.com/user/?aad=[col_value]&tid={base_object.TenantId}" target="_blank">[col_value]</a>'
+        link_template = f'<a href="https://security.microsoft.com/user?aad=[col_value]&tid={base_object.TenantId}" target="_blank">[col_value]</a>'
         linked_table = data.update_column_value_in_list([{k: v for k, v in DetailedResults.items() if k != 'ThreatScoreHistory'} for DetailedResults in mdac_object.DetailedResults], 'UserId', link_template)
         html_table = data.list_to_html_table(linked_table, escape_html=False)
         comment = f'<h3>Microsoft Defender for Cloud Apps Module</h3>'
         comment += f'A total of {mdac_object.AnalyzedEntities} entities were analyzed.<br />'
         comment += f'<ul><li>Maximum investigation score: {mdac_object.MaximumScore}</li>'
-        comment += f'<li>Users above the score threshold of {ScoreThreshold}: {mdac_object.AboveThresholdCount} </li></ul><br />'
+        comment += f'<li>Highest percentile: {mdac_object.HighestScorePercentile}</li>'
+        comment += f'<li>Users above the score threshold of {ScoreThreshold}: {mdac_object.AboveThresholdCount} </li>'
+        comment += f'<li>Any users trending up: {mdac_object.AnyThreatScoreTrendingUp} </li>'
+        comment +=</ul><br />'
         comment += f'{html_table}'
         comment_result = rest.add_incident_comment(base_object, comment)
 
