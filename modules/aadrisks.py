30a15359e20b8d70bb3bb05b0a9ba8cf15ad6561
Brian Delaney <briandel@microsoft.com>
2024-04-18 12:53:21 -0400

    rename to entra id

--- modules/aadrisks.py
+++ modules/aadrisks.py
@@ -103,6 +103,6 @@ def execute_aadrisks_module (req_body):
         comment_result = rest.add_incident_comment(base_object, comment)
 
     if req_body.get('AddIncidentTask', False) and (data.coalesce(aadrisks_object.FailedMFATotalCount,0) > 0 or data.coalesce(aadrisks_object.MFAFraudTotalCount,0) > 0 or data.coalesce(aadrisks_object.SuspiciousActivityReportTotalCount,0) > 0 or data.coalesce(aadrisks_object.RiskDetectionTotalCount,0) > 0 or ( aadrisks_object.HighestRiskLevel != 'None' and aadrisks_object.HighestRiskLevel != 'Unknown')):
-        task_result = rest.add_incident_task(base_object, req_body.get('QueryDescription', 'Review users Azure AD risks level, MFA failures and fraud reports.'), req_body.get('IncidentTaskInstructions'))
+        task_result = rest.add_incident_task(base_object, req_body.get('QueryDescription', 'Review users Entra ID risk level, MFA failures and fraud reports.'), req_body.get('IncidentTaskInstructions'))
 
     return Response(aadrisks_object)
